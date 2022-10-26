bbc3ec834759f779c66cfa2342e6ffbb3bb692c1
jamshidsalayev1998 <jmlaravel@mail.ru>
2022-10-26 20:07:56 +0500

    jhg

---
+++ resources/views/student/agreement/join_training/agreements/includes/style_pdf.blade.php
@@ -0,0 +1,54 @@
+<style>
+
+    .text-bold {
+        font-weight: bold;
+        /*font-weight: bolder;*/
+    }
+
+    * {
+        font-family: "Times New Roman";
+        font-size: 11px;
+    }
+
+    /*body{*/
+    /*    padding-left: 30px;*/
+    /*    padding-right: 30px;*/
+    /*}*/
+    .mt-1 {
+        margin-top: 7px;
+    }
+
+    .mb-1 {
+        margin-bottom: 7px;
+    }
+
+    .word-line {
+        /*padding-top: 30px;*/
+        border-bottom: 1px solid black
+    }
+
+    td {
+        vertical-align: top;
+        padding: 7px;
+    }
+
+    .page-break {
+        page-break-after: always;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    .text-right {
+        display: inline-block;
+        width: 49%;
+        text-align: right;
+    }
+
+    .text-left {
+        display: inline-block;
+        width: 49%;
+
+    }
+</style>
