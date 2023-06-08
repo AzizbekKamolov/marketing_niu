745ac4f4c77130ad575f5662d6291fc919a762b5
AzizbekKamolov <96434885+AzizbekKamolov@users.noreply.github.com>
2023-06-08 17:34:29 +0500

    sdf

--- resources/views/site/includes/footer2021.blade.php
+++ resources/views/site/includes/footer2021.blade.php
@@ -13,18 +13,16 @@ L<footer class="footer">
 
             <div class="col-12 col-md-6 col-lg-4 col-xl-4 text-sm-center text-start mb-2">
                 <p class="colorThree fs-2 m-0">
-                    Angren Universiteti
+                    Navoiy Innovatsiyalar Universitet
                 </p>
-                <p class="colorThree m-0"><strong class="fas fa-phone-alt"></strong><b>&nbsp;:</b>&nbsp    +998 (94) 722-62-68</p>
-                <p class="colorThree m-0"><strong class="fas fa-envelope"></strong><b>&nbsp;:</b>&nbsp;xaydarov_farxod@mail.ru</p>
+                <p class="colorThree m-0"><strong class="fas fa-phone-alt"></strong><b>&nbsp;:</b>&nbsp    (55) 500-00-43</p>
+                <p class="colorThree m-0"><strong class="fas fa-envelope"></strong><b>&nbsp;:</b>&nbsp;info@niuedu.uz</p>
             </div>
 
             <div class="col-12 col-md-6 col-lg-4 col-xl-4 mb-2">
-                <p class="colorThree m-0 text-start"><i class="fas fa-map-marker-alt fs-5"></i> <b>:</b> O‘zbekiston
-                    Respublikasi, Toshkent viloyati, Angren shahri, Ozodlik MFY,
-                    Fleyshmaxer ko’chasi, 2-uy.</p>
+                <p class="colorThree m-0 text-start"><i class="fas fa-map-marker-alt fs-5"></i> <b>:</b> Navoiy viloyati Karmana tumani Toshkent ko'chasi 39-uy</p>
                 <p class="text-start m-0"><a class="colorFour text-decoration-none"
-                                             href="https://auni.uz" target="_blank"><i
+                                             href="https://niuedu.uz/uz/" target="_blank"><i
                                 class="fas fa-sign-in-alt fs-5 mx-2 mt-2"></i>Loyiha
                         haqida</a></p>
             </div>
